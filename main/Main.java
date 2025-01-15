6f0557f0f54b3d199111a511b0328c6683d8d2c7
Chin Wei Kang <89310598+kang003417@users.noreply.github.com>
2025-01-15 21:22:02 +0800

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,14 @@
+package main;
+import registration.UserRegistration;
+import controller.LoginController;
+import util.DatabaseUtil;
+import view.LoginView;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Start with the Login page
+        new LoginView();
+    }
+}
+
